2603a2437fca4aace238ac3d568828875e2543ce
Zhang Dian <54255897+zdpcdt@users.noreply.github.com>
2023-03-10 22:28:45 +0800

    feat: add carousel resource

--- CarouselDemo/MainWindow.axaml.cs
+++ CarouselDemo/MainWindow.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace CarouselDemo;
 
@@ -7,5 +8,15 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void Button_OnClick(object? sender, RoutedEventArgs e)
+    {
+        carouse.Previous();
+    }
+    
+    private void Button_OnClick2(object? sender, RoutedEventArgs e)
+    {
+        carouse.Next();
     }
 }
